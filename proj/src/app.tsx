346323a8137047244ee58ff95e0411601d9bebea
yqxue <xueyiqiang909@163.com>
2018-02-27 16:45:55 +0800

    完成配置

--- proj/src/app.tsx
+++ proj/src/app.tsx
@@ -2,14 +2,16 @@ import * as React from "react";
 
 import {Hello} from "./components/Hello";
 import Greeting from "./components/greeting";
+export interface Props extends React.Props < App > {
 
+}
 
-export default class App extends React.Component < {}, {} > {
+export default class App extends React.Component < Props, {} > {
     render() {
         return (
             <div>
                 <Hello compiler = "TypeScript" framework = "React" />
-                {/* <Greeting name={'lily'}/> */}
+                <Greeting name={'lily'} enthusiasmLevel={4}/>
             </div>
         )
     }
--- proj/webpack.config.js
+++ proj/webpack.config.js
@@ -1,61 +1,66 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: "./src/index.tsx",
-    
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, 'dist')
-    },
+  entry: './src/index.tsx',
 
-    // Enable sourcemaps for debugging webpack's output. devtool: "source-map",
-    devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
 
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist')
-    },
+  // Enable sourcemaps for debugging webpack's output. devtool: "source-map",
+  devtool: 'inline-source-map',
 
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'Development',
-            filename: 'index.html',
-            template: __dirname + '/index.html'
-        })
-    ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  },
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      filename: 'index.html',
+      template: __dirname + '/index.html'
+    })
+  ],
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by
-            // 'awesome-typescript-loader'.
-            {
-                test: /\.tsx?$/,
-                loader: "awesome-typescript-loader"
-            },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
 
-            // All output '.js' files will have any sourcemaps re-processed by
-            // 'source-map-loader'.
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
-            }
-        ]
-    },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by
+      // 'awesome-typescript-loader'.编译目前有问题  https://github.com/s-panferov/awesome-typescript-loader/issues/541
+      // {
+      //     test: /\.tsx?$/,
+      //     loader: "awesome-typescript-loader"
+      // }, 
 
-    // When importing a module whose path matches one of the following, just assume
-    // a corresponding global variable exists and use that instead. This is
-    // important because it allows us to avoid bundling all of our dependencies,
-    // which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    }
-};
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+
+      // All output '.js' files will have any sourcemaps re-processed by
+      // 'source-map-loader'.
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+
+  // When importing a module whose path matches one of the following, just assume
+  // a corresponding global variable exists and use that instead. This is
+  // important because it allows us to avoid bundling all of our dependencies,
+  // which allows browsers to cache those libraries between builds.
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+  }
+}
